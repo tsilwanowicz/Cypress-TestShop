6d1f46adde9b37df8d737141946372567aaeeee2
tsilwanowicz <34251568+tsilwanowicz@users.noreply.github.com>
2023-11-20 13:51:43 +0100

    Update TS3.cy.js

--- cypress/e2e/TS3.cy.js
+++ cypress/e2e/TS3.cy.js
@@ -1,25 +1,45 @@
 import HomePage from "../page_objects/homePage";
 import MyAccountPage from "../page_objects/myAccountPage";
 import CartPage from "../page_objects/cartPage";
+import ShopPage from "../page_objects/shopPage";
 import { faker } from '@faker-js/faker';
 
 describe('my first scenario', () => {
     const homePage = new HomePage();
     const myAccountPage = new MyAccountPage();
     const cartPage = new CartPage();
+    const shopPage = new ShopPage();
 
     before(function (){
-        cy.fixture('users').as('userData')
+        cy.fixture('products').as('productData')
     })
 
-    it('should add product to cart & then remove it from cart', function() {
+    it('should add product to cart & then remove it from cart', function(){
         homePage.visitPage()
-        homePage.addProductToCart()
+        homePage.addProductToCart(this.productData.Polo)
         homePage.clickGoToCartFromProductButton()
-        cartPage.checkIfAddedProductIsInCart()
-        cartPage.removeProductFromCart()
+        cartPage.checkIfAddedProductIsInCart(this.productData.Polo)
+        cartPage.removeProductFromCart(this.productData.Polo)
         cartPage.checkIfCartIsEmpty()
 
+    })
+    
+    it('should change sorting of products', function(){
+        shopPage.visitPage()
+        shopPage.changeSortingBy()
+    })
+
+    it('should click product thumbnail & check product tabs', function(){
+        shopPage.visitPage()
+        shopPage.clickProductThumbnail('Hoodie')
+        shopPage.checkProductPageUrl('Hoodie')
+        shopPage.checkProductDetailsPage()
+    })
+
+    it('should write a product review', function(){
+        shopPage.visitPage()
+        shopPage.clickProductThumbnail('Beanie')
+        shopPage.writeValidProductReview()
     })
 
 })
