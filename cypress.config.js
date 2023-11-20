77c493d8b6fd7ba01d7528daf688733a40094ef5
tsilwanowicz <34251568+tsilwanowicz@users.noreply.github.com>
2023-11-20 13:54:34 +0100

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,9 @@ module.exports = defineConfig({
 
   viewportHeight: 800,
   viewportWidth: 1200,
-
   e2e: {
     baseUrl: 'https://tapsshop.pl/',
-  }
+    setupNodeEvents(on, config) {
+    },
+  },
 });
