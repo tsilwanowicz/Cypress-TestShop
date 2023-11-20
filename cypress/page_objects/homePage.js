cafc0652bc321943a44f1c4a9cbcf722aaba1259
tsilwanowicz <34251568+tsilwanowicz@users.noreply.github.com>
2023-11-20 13:52:39 +0100

    Update homePage.js

--- cypress/page_objects/homePage.js
+++ cypress/page_objects/homePage.js
@@ -1,13 +1,20 @@
 import Urls from "./urls";
 
-const MyAccountHeaderButton = '#menu-item-100'
+const myAccountHeaderButton = '#menu-item-100'
 const dataBlockNew = 'div[data-block-name="woocommerce/product-new"]'
-const CheckCartFromProductLevel = '.added_to_cart.wc-forward'
+const checkCartFromProductLevel = '.added_to_cart.wc-forward'
+const mainPageBanner = '.wp-block-cover__background'
+const categoryHeader = '.wp-block-heading'
+const searchBar = '#woocommerce-product-search-field-0'
+const searchResults = '.woocommerce-products-header'
+const searchResultsColumn = '.products.columns-4'
+const productName = '.product_title.entry-title'
+const addToCartButtonSingle = 'button[name="add-to-cart"]'
 
 class HomePage{
 
     clickMyAccountHeaderButton(){
-        cy.get(MyAccountHeaderButton).click()
+        cy.get(myAccountHeaderButton).click()
     }
 
     addProductToCart(Polo){
@@ -18,13 +25,40 @@ class HomePage{
 
     clickGoToCartFromProductButton(){
         cy.get(dataBlockNew).within(()=>{
-            cy.get(CheckCartFromProductLevel).click()
+            cy.get(checkCartFromProductLevel).click()
         })
+    }
+
+    checkMainPageBanner(){
+        cy.get(mainPageBanner).should('be.visible')
+    }
+
+    checkCategoryHeader(categoryHeaderName){
+        cy.get(categoryHeader).contains(categoryHeaderName).should('be.visible')
     }
 
     visitPage(){
         const urls = new Urls
         urls.visitHomePage()
+    }
+
+    checkHomePageUrl(){
+        cy.url().should('eq', 'https://tapsshop.pl/')
+    }
+
+    checkSearchBarWithMultipleResults(searchTerm){
+        cy.get(searchBar).should('be.visible').type(searchTerm).type("{enter}")
+        cy.get(searchResults).should('be.visible').contains(searchTerm)
+        cy.get(searchResultsColumn).should('be.visible').within(() => {
+            cy.get('.woocommerce-loop-product__title').contains(searchTerm)
+        })
+    }
+
+    checkSearchBarWithSingleResult(searchTerm){
+        cy.get(searchBar).should('be.visible').type(searchTerm).type("{enter}")
+        cy.get(productName).should('be.visible').contains(searchTerm)
+        cy.get(addToCartButtonSingle).should('be.visible')
+
     }
 }
 
